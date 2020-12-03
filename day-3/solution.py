f6f5cb132b6b0c73507aee98675e39117da30408
Danny Fraser <danrfraser@gmail.com>
2020-12-03 10:30:35 +0000

    started looking at day 3

---
+++ day-3/solution.py
@@ -0,0 +1,10 @@
+with open("inputs.txt") as f:
+    inputs = f.readlines()
+
+with open("test_inputs.txt") as f:
+    test_inputs = f.readlines()
+
+def solve_1(inputs: List[str]) -> int:
+    ...
+
+assert solve_1(test_inputs) == 7
